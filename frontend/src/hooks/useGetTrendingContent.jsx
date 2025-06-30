8a0cef3d382d0c32119bf441808d916d42d2c378
Hady Essa <hadyessa585@gmail.com>
2025-06-30 19:37:34 +0300

    Refactor content fetching and display; implement MovieSlider component and enhance HomeScreen layout

--- frontend/src/hooks/useGetTrendingContent.jsx
+++ frontend/src/hooks/useGetTrendingContent.jsx
@@ -2,20 +2,19 @@ import { useEffect, useState } from "react";
 import { useContentStore } from "../store/content";
 import axios from "axios";
 
-const useGetTrandingContent = () => {
-  const [trendingContent, setTrendingContent] = useState([]);
+const useGetTrendingContent = () => {
+  const [trendingContent, setTrendingContent] = useState(null);
   const { contentType } = useContentStore();
 
   useEffect(() => {
-    const getTrandingContent = async () => {
+    const getTrendingContent = async () => {
       const res = await axios.get(`/api/v1/${contentType}/trending`);
       setTrendingContent(res.data.content);
     };
 
-    getTrandingContent();
+    getTrendingContent();
   }, [contentType]);
 
-  return trendingContent;
+  return { trendingContent };
 };
-
-export default useGetTrandingContent;
+export default useGetTrendingContent;
