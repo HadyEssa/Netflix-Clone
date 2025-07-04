f33084a7c94ea604ef51dade5878bc3e6dd7858c
Hady Essa <hadyessa585@gmail.com>
2025-07-04 21:35:53 +0300

    fix import auth

--- frontend/src/pages/SignUpPage.jsx
+++ frontend/src/pages/SignUpPage.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { Link } from "react-router-dom"
-import { useAuthUserStore } from "../store/AuthUser";
+import { useAuthUserStore } from "../store/authUser";
 
 const SignUpPage = () => {
   const {searchParams} = new URL(document.location);
